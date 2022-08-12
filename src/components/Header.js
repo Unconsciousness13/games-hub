da9464bebecbb7f51f302fa4e8909549fb27c1c3
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-08-12 14:33:03 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 const Header = ({ active, setActive, user, handleLogout }) => {
   const userId = user?.uid;
+  const userPhoto = user?.photoURL;
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid bg-dark padding-media">
@@ -29,9 +32,8 @@ const Header = ({ active, setActive, user, handleLogout }) => {
                   <>
                     <Link to="/" style={{ textDecoration: "none" }}>
                       <li
-                        className={`nav-item nav-link ${
-                          active === "home" ? "active" : ""
-                        }`}
+                        className={`nav-item nav-link ${active === "home" ? "active" : ""
+                          }`}
                         onClick={() => setActive("home")}
                       >
                         Home
@@ -40,9 +42,8 @@ const Header = ({ active, setActive, user, handleLogout }) => {
 
                     <Link to="/create" style={{ textDecoration: "none" }}>
                       <li
-                        className={`nav-item nav-link ${
-                          active === "create" ? "active" : ""
-                        }`}
+                        className={`nav-item nav-link ${active === "create" ? "active" : ""
+                          }`}
                         onClick={() => setActive("create")}
                       >
                         Create
@@ -51,32 +52,31 @@ const Header = ({ active, setActive, user, handleLogout }) => {
 
                     <Link to="/mygames" style={{ textDecoration: "none" }}>
                       <li
-                        className={`nav-item nav-link ${
-                          active === "mygames" ? "active" : ""
-                        }`}
+                        className={`nav-item nav-link ${active === "mygames" ? "active" : ""
+                          }`}
                         onClick={() => setActive("mygames")}
                       >
                         My Games
                       </li>
-                      
+
                     </Link>
+
                     <img style={{
-                            display: "block",
-                            position: "absolute",
-                            marginLeft: "45%",
-                            width: "180px",
-                            height: "30px",
-                            marginTop: "10px",
-                          }} alt="logo" src="https://i.ibb.co/VCPSmYg/logo.png" />
+                      display: "block",
+                      position: "absolute",
+                      marginLeft: "45%",
+                      width: "180px",
+                      height: "30px",
+                      marginTop: "10px",
+                    }} alt="logo" src="https://i.ibb.co/VCPSmYg/logo.png" />
                   </>
-                  
+
                 ) : (
                   <>
                     <Link to="/" style={{ textDecoration: "none" }}>
                       <li
-                        className={`nav-item nav-link ${
-                          active === "home" ? "active" : ""
-                        }`}
+                        className={`nav-item nav-link ${active === "home" ? "active" : ""
+                          }`}
                         onClick={() => setActive("home")}
                       >
                         Home
@@ -85,35 +85,61 @@ const Header = ({ active, setActive, user, handleLogout }) => {
                   </>
                 )}
               </ul>
-              <div className="row g-3">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <div className="row g-3" >
+
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
                   {userId ? (
                     <>
-                      <div className="profile-logo">
-                        <img
-                          src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-                          alt="profile"
-                          style={{
-                            width: "30px",
-                            height: "30px",
-                            borderRadius: "50%",
-                            marginTop: "12px",
-                          }}
-                        />
-                      </div>
-                      <p style={{ marginTop: "12px", marginLeft: "5px" }}>
+                      <Link to="/editprofile" className="hover-link" style={{ textDecoration: "none" }}>
+                        <div className="profile-logo">
+                          {userPhoto ? (
+                            <img
+                              src={userPhoto}
+                              alt="profile"
+                              style={{
+                                width: "30px",
+                                height: "30px",
+                                borderRadius: "50%",
+                                marginTop: "12px",
+                              }}
+                            />
+                          ) :
+                            (
+                              <img
+                                src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                                alt="profile"
+                                style={{
+                                  width: "30px",
+                                  height: "30px",
+                                  borderRadius: "50%",
+                                  marginTop: "12px",
+                                }}
+                              />
+                            )
+
+                          }
+
+
+
+                        </div>
+                      </Link>
+
+                      <p style={{ marginTop: "12px", marginLeft: "5px", color: "white", textShadow: "0.5px 0.5px blue" }}>
                         {user?.displayName}
+
                       </p>
+
+
                       <li className="nav-item nav-link" onClick={handleLogout}>
                         Logout
                       </li>
+
                     </>
                   ) : (
                     <Link to="/auth" style={{ textDecoration: "none" }}>
                       <li
-                        className={`nav-item nav-link ${
-                          active === "login" ? "active" : ""
-                        }`}
+                        className={`nav-item nav-link ${active === "login" ? "active" : ""
+                          }`}
                         onClick={() => setActive("login")}
                       >
                         Login
